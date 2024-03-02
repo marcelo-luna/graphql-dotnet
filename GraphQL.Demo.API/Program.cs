c7aa84bef173ad4508c3fabac8a520af951bf5f0
Luna Marcelo <marcelo.1.luna@gmail.com>
2024-03-02 13:31:51 +0100

    Add project and hotchocolate for aspnet app

---
+++ GraphQL.Demo.API/Program.cs
@@ -0,0 +1,27 @@
+using GraphQL.Demo.API.Schema;
+
+namespace GraphQL.Demo.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddGraphQLServer().AddQueryType<Query>();
+
+            var app = builder.Build();
+
+            //app.MapGet("/", () => "Hello World!");
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints => 
+            {
+                endpoints.MapGraphQL();
+            });
+
+            app.Run();
+        }
+    }
+}
---
+++ GraphQL.Demo.API/Schema/Query.cs
@@ -0,0 +1,7 @@
+﻿namespace GraphQL.Demo.API.Schema
+{
+    public class Query
+    {
+        public string Instructions { get; } = "Struction to be added in query";
+    }
+}
